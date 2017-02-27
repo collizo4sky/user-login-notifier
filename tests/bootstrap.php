31a14a53a32a62766aae492e03137b1aeb07af9b
Collins Agbonghama <collizo4sky@gmail.com>
2017-02-27 13:24:23 +0100

    lots of changes after a lot of time

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,9 +17,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname(dirname(__FILE__)) . '/vendor/autoload.php';
-	
-	require dirname( dirname( __FILE__ ) ) . '/user-login-notifier.php';
+    require dirname( dirname( __FILE__ ) ) . '/user-login-notifier.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
